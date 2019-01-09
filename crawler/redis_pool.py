3f9089d2651a2e22836b15ba9c89c8d19f125f70
daixinye <denight@qq.com>
2019-01-09 21:56:47 +0800

    format code

--- crawler/redis_pool.py
+++ crawler/redis_pool.py
@@ -12,9 +12,12 @@ KEY_USED = "yonghuchi_used"
 redisClient = redis.Redis(host=HOST, port=PORT, password=PASS)
 
 # 加入用户到用户池中
+
+
 def addUser(userId):
     # 判断是否在已爬过的用户列表中
     if not redisClient.sismember(KEY_USED, userId):
+        print("redis_pool: 添加用户" + userId)
         return redisClient.sadd(KEY, userId)
 
 
@@ -23,21 +26,31 @@ def getRandomUser():
     return redisClient.spop(KEY)
 
 # 获取用户列表
+
+
 def getUsers():
     return redisClient.smembers(KEY)
 
 # 移除用户
+
+
 def removeUser(userId):
     return redisClient.srem(KEY, userId)
 
 # 获取随机用户并加入到已爬用户列表中
+
+
 def getRandomUserAndRemove():
     userId = getRandomUser()
-    addUserToUsed(userId)
-    return userId
+    if userId:
+        addUserToUsed(userId)
+        return userId
+    return False
+
 
 def addUserToUsed(userId):
     return redisClient.sadd(KEY_USED, userId)
+
 
 def getUsersUsed():
     return redisClient.smembers(KEY_USED)
@@ -45,9 +58,10 @@ def getUsersUsed():
 
 if __name__ == '__main__':
     temp = time.time()
-    print(addUser(temp))
-    print(getUsers())
-    print(getUsersUsed())
     print(getRandomUserAndRemove())
-    print(getUsers())
-    print(getUsersUsed())
+#     print(addUser(temp))
+#     print(getUsers())
+#     print(getUsersUsed())
+#     print(getRandomUserAndRemove())
+#     print(getUsers())
+#     print(getUsersUsed())
